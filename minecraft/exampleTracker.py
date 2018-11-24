b0dbd3650162b29e0d57022086dcdf5e55a17400
robfatland <rob5@uw.edu>
2018-11-23 22:47:49 -0800

    minecraft python mods

--- minecraft/exampleTracker.py
+++ minecraft/exampleTracker.py
@@ -1,6 +1,8 @@
-# geometry_helper contains simple or 'primitive' geometric builders
-#
-# pos = mc.player.getPos() # to set type (inelegant)
+# This program does not work as intended
+#   The intent is to create a ring in the forward direction that the player can fly through
+#   While this does not work the code still tracks the players motion tracing her path
+#     in the air with blocks
+
 
 from mcpi.minecraft import Minecraft
 from mcpi.vec3 import Vec3
@@ -63,49 +65,44 @@ def LineSegment(mc, a, b, block):
 
 polling_interval = 0.4
 ahead_distance = 20.
-ahead_radius = 10.0
-nArcBlocks = int(ahead_radius * 2. * 3.14159 * 1.2)    # 1.2 is a factor to oversample a bit
-        
+
+gold = 41
+TNT = 46
+redstone = 152
+counter = 0
 while True:
+    
     time.sleep(polling_interval)
-    p2 = mc.player.getPos()
+    
+    p2 = mc.player.getPos()           # p2 is position 2, the current player position
+
+    counter += 1
+    
+    # Only if the player has moved from the previous fix do we proceed
     if np.abs(p2.x - p.x) > 2. or np.abs(p2.y-p.y) > 2. or np.abs(p2.z-p.z) > 2.:
 
         # This draws a trailing line
         # LineSegment(mc, p, p2, (49, 0))
         # p = p2
 
-        # v will be a unit vector in the direction of motion
+        # v will be a vector in the direction of motion of length 'ahead_distance'
         v = p2 - p
         mag_v = np.sqrt(v.x*v.x + v.y*v.y + v.z*v.z)
-        v.x /= mag_v
-        v.y /= mag_v
-        v.z /= mag_v
+        v.x *= ahead_distance/mag_v
+        v.y *= ahead_distance/mag_v
+        v.z *= ahead_distance/mag_v
 
-        # cc is a location of the center of the drawing circle in mc space
-        cc = p2 + Vec3(v.x*ahead_distance, v.y*ahead_distance, v.z*ahead_distance)    # location of center of circle
-
-        # yz is unit perpendicular to v with a zero x-component
-        yz = Vec3(1., 0., -v.x/v.z)
-        mag_yz = np.sqrt(yz.x*yz.x + yz.y*yz.y + yz.z*yz.z)
-        yz.x /= mag_yz
-        yz.y /= mag_yz
-        yz.z /= mag_yz
-
-        # up is unit perpendicular to v and yz
-        up = Vec3(yz.y*v.z-yz.z*v.y, yz.z*v.x-yz.x*v.z, yz.x*v.y-yz.y*v.x)
-        mag_up = np.sqrt(up.x*up.x + up.y*up.y + up.z*up.z)
-        up.x /= mag_up
-        up.y /= mag_up
-        up.z /= mag_up
+        # cc is 'circle center': a location forward of the player
+        cc = p2 + Vec3(v.x, v.y, v.z)
 
         for i in range(nArcBlocks):
-            theta = (float(i)/30.)*2.*np.pi
-            ct = np.cos(theta)
-            st = np.sin(theta)
-            pp = cc + Vec3(yz.x*ct, yz.y*ct, yz.z*ct) + Vec3(up.x*st, up.y*st, up.z*st)
-            mc.setBlock(pp.x, pp.y, pp.z, 46)
-            
+            if counter % 10 == 0:
+                mc.setBlock(cc.x, cc.y-1, cc.z, redstone)
+                mc.setBlock(cc.x, cc.y, cc.z, TNT, 1)
+            else:
+                mc.setBlock(cc.x, cc.y, cc.z, gold)
+
+    if counter > 500: break
         
 
     
