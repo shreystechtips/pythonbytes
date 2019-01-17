5d04b0ca7294cf421d8a02a3e9ed2f53683a33e9
robfatland <rob5@uw.edu>
2019-01-17 14:54:47 -0800

    jake done ish

---
+++ projects/idunno/ocean.py
@@ -0,0 +1,10 @@
+zero_row = [0]*400
+print(type(zero_row))
+ocean = [zero_row]*400
+ocean[137][219]=18
+
+print(len(ocean))
+
+print(ocean[137])
+print(ocean[137][219])
+print(ocean[137][220])
--- projects/sudoku/sudoku_jake.py
+++ projects/sudoku/sudoku_jake.py
@@ -1,4 +1,14 @@
-# index on [0, 80] produces lists of indices for that row / column / block
+# In this program we follow Jake's example code very closely. We use the Python set
+#   because it is very well suited to solving Sudoku. A set is different from a list
+#   in two important ways: First the elements of a set occur only one time. Second
+#   the elements of a set are not in any particular order.
+#
+# In this program the sets are always sets of characters (not integers). These
+#   characters are '0', '1', '2', '3', '4', '5', '6', '7', '8', and '9'.
+#
+# We also use something called a tuple; in our case a 3-tuple. The 3-tuple used
+#   here represents the possible characters (1, 2, ..., 9) that could go in an
+#   empty cell of the puzzle. The tuple is described below. 
 
 def row_indices(i): return range(i - i % 9, i - i % 9 + 9)
 
@@ -8,64 +18,80 @@ def box_indices(i):
     start = 27 * (i // 27) + 3 * ((i % 9) // 3)
     return [i for j in range(3) for i in range(start + 9 * j, start + 9 * j + 3)]
 
-# connected[] is a list of sets { ... }, in fact 81 of them where the list index is the cell index
-#   The set for a given index is a set that is the union of three sets... rule-connect cell indices...
-#     ...with the index i itself excluded
-connected = [(set.union(set(box_indices(i)), set(row_indices(i)), set(col_indices(i))) - set([i]))
-             for i in range(81)]
+# connected[] is a list of 81 sets { ... } where the list index i is the cell index.
+#   Each set contains indices of other cells that -- when populated with a number from
+#   1 to 9 -- act as excluder values: Values that may not be placed in cell i. 
+connected = [(set.union(set(box_indices(i)), set(row_indices(i)), set(col_indices(i))) - set([i])) for i in range(81)]
 
 solution_count = 0
 
 # solver(p) will recursively find solutions and "yield" them
-#   A key idea here is that the 'p' this is passed the first time is a precise copy of the original puzzle
-#   but subsequent (recursive) calls to solver() will provide guess-versions of the puzzle that have
-#   additional squares filled in. In this way eventually solver(p) will be -- we hope -- passed a complete
-#   solution built up from the original puzzle. When this happens there are two logical paths for the
-#   code:
-#     Test for this immediately; and celebrate!
-#     Build a picture of possible moves first; then test and *yield*
+#   Key idea 1: The 'p' passed to solver(p) is at first the original puzzle (as a string). 
+#   Later (recursive) calls to solver(p) will provide guess-versions of p with more of the
+#   cells filled in. In this way -- eventually, if all goes well -- solver(p) will be passed
+#   a correct solution to the original puzzle. When this happens we should notice and celebrate!
+#
+#   Key idea 2: When solver(p) receives an incomplete version of the puzzle p it should add
+#     one more guess to that puzzle. That is, fill in one empty square with a guess. Here is how:
+#       - Build a picture of possible moves first; then test and *yield*
+
 def solver(p):
 
     global solution_count
-    if '0' not in p:
-        print('found', solution_count, ':', p)
-        solution_count += 1
-        if solution_count > 20: return
+
+    # First thing we could do here is check to see if the puzzle is solved by 'p'
+    #   The question is: What is sufficient logic to be sure that our puzzle is ok?
+    # if '0' not in p:
+    #     print('found', solution_count, ':', p)
+    #     solution_count += 1
+    #     if solution_count > 20: return
 
     
-    # Accumulate a list L[] of triples across empty squares
-    #   Each triple is(number of possible guesses, this square index, a set of those guesses)
+    # Accumulate a list L[] of possibilities: Possible next moves in the puzzle
+    #   Each possibility is a 3-tuple:
+    #     (number of possible guesses, cell index, the set of those possible guesses)
     L = []
     for i in range(81):
         if p[i] == '0':
-            vals = set('123456789') - set(p[n] for n in connected[i])
-              # working backwards: connected[i] is a set of related cells to cell i
-              #   For example for i = 10 we have these connected cell indices:
-              #   { 0,1,2,9,10,11,12,13,14,15,16,17,18,19,20,28,37,46,55,64,73}
-              #   for n in connected[i] has n scan all these puzzle indices: 0,1,2,9,...,73
-              #   p[n] is the puzzle value at that index, e.g. '0' or '3' or '4'
-              #   set() makes a set of these puzzle values without any duplicates
-              #   set('123456789') - set() removes these puzzle values from the set
-              #     of allowed guesses; producing a set of properly constrained allowed guesses.
-              # vals is a set of legal guesses for this square in this call of solver()
-            if len(vals) == 0: return
-              # if solver has found a square with no legal guesses it simply returns None
-              #   We want to bail out of solver() if a cell is a dead end
-            else: L.append((len(vals), i, vals))
-              # otherwise contribute to the L[] list of triples as described above
+            vals = set(['1', '2', '3', '4', '5', '6', '7', '8', '9']) - set(p[n] for n in connected[i])
+              # working backwards: connected[i] is a set of cells connected to cell i
+              # p[n] is the puzzle value at a connected cell (0 or one of {1, 2, ..., 9})
+              # set(--etc--) makes a set of excluded possible values at cell i
+              # set('123456789') - set(--etc--) is a set of *allowable* guesses at cell i: vals
+            if len(vals) == 0: return             # there are no possible values for this empty cell
+            else: L.append((len(vals), i, vals))  # expand the 'possibilities' list L
     
-    # if all squares are solved, then yield the current solution
-    #   Notice this is done only *after* L is constructed... why???
+    # if all squares are solved: yield the current puzzle (which must be a solution)
     if len(L) == 0 and '0' not in p: yield p
         
     # choose a cell (index) with the smallest number of possibles; recursively call S() for each possible
     else:
-        N, i, vals = min(L)   # that is a triple based on the first minimum N in L[]
-        for val in vals:      # try all possible allowed guesses for this cell...
-            for s in solver(p[:i] + val + p[i + 1:]): yield s  # ...and for each return all solutions
+        N, i, vals = min(L)   # the guess cell will be the one with the smallest set of possible guesses
+        for guess in vals:      #   ...and let's be thorough and try them all
+            puzzle_with_new_guess = p[:i] + guess + p[i+1:]       # this glues together 3 strings
+            for s in solver(puzzle_with_new_guess):
+                yield s     # ...and for each return all solutions
+
+
+def CheckSolution(a):
+    checkset = set('123456789')
+    # row check
+    for i in range(9):
+        bc = (i%3)*3 + (i//3)*27
+        row = set([a[i*9+j] for j in range(9)])
+        col = set([a[i + 9*j] for j in range(9)])
+        box = set([a[bc], a[bc+1], a[bc+2], a[bc+9], a[bc+10], a[bc+11], a[bc+18], a[bc+19], a[bc+20]])
+        if not (row == checkset and col == checkset and box == checkset):
+            print('NOT A SOLUTION!!!')
+            return
+    print('SOLUTION OK')
+    return
 
 p0 = '000000000000000000000000000000000000000000000000000000000000000000000000000000000'
 p1 = '060000008201800500905002000000704602300090005504608000000900301007005906100000050'
+p2 = '008632400040000010500904006800000005600000004107000902400751003060000020005826700'
+allsolns = solver(p2)
+for a in allsolns:
+    print(a)
+    CheckSolution(a)
 
-allsolns = solver(p0)
-for a in allsolns: print(a)
